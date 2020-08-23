d660b90720d3fefdecb6e26d21039fbae7bcd69c
Aung Thuya Oo <thuyaoo200@gmail.com>
2020-08-24 05:25:28 +0800

    Remove secret.ts

--- src/secret.ts
+++
@@ -1,7 +0,0 @@
-// Edit your Ids here
-const botToken = '1115397540:AAHagR3oMvHCJfRGS0t24t5qf5_Ftslmmt0';
-const botName = '@lesson222bot';
-const deploymentId =
-  'AKfycbzzQdz7fs6MEQJb6moVI_9yrZRnJyJDgIyxTYiobhX9eqUSPmq1JLq9o_VhIdyBnxPi';
-const breakfastMenuSheetId = '1ICEDGrrlGP9FAldnirTGba6JRg0qZ0VJVa_7nN32gsI';
-const dinnerMenuSheetId = '1bE4zi9LHjq1lSf9exWPe9O8wLitCyW_jS0UQSy6t2PU';
